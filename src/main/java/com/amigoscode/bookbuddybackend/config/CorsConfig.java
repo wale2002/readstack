12d810f2c3ef7b3c1cc55e7d301a8773417e9f06
wale2002 <gemred2002@gmail.com>
2025-11-22 17:22:07 +0100

    addcors

--- src/main/java/com/amigoscode/bookbuddybackend/config/CorsConfig.java
+++ src/main/java/com/amigoscode/bookbuddybackend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000"); // Frontend origin
+        config.addAllowedOrigin("https://readstackk.vercel.app"); // Frontend origin
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
